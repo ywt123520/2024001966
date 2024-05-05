3178b6b15573bd9c9a4b3b1cf3f042e62afffbd4
ywt123520 <134662493+ywt123520@users.noreply.github.com>
2024-05-05 19:28:53 +0800

    Add files via upload

---
+++ test/cultural/js/js.js
@@ -0,0 +1,797 @@
+﻿
+$(function () {
+echarts_1();
+echarts_2();
+echarts_4();
+echarts_31();
+echarts_32();
+echarts_33();
+echarts_5();
+echarts_6();
+function echarts_1() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echart1'));
+
+       option = {
+  //  backgroundColor: '#00265f',
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    grid: {
+        left: '0%',
+		top:'10px',
+        right: '0%',
+        bottom: '4%',
+       containLabel: true
+    },
+    xAxis: [{
+        type: 'category',
+      		data: ['2015', '2016', '2017', '2018', '2019', '2020'],
+        axisLine: {
+            show: true,
+         lineStyle: {
+                color: "rgba(255,255,255,.1)",
+                width: 1,
+                type: "solid"
+            },
+        },
+		
+        axisTick: {
+            show: false,
+        },
+		axisLabel:  {
+                interval: 0,
+               // rotate:50,
+                show: true,
+                splitNumber: 15,
+                textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+    }],
+    yAxis: [{
+        type: 'value',
+        axisLabel: {
+           //formatter: '{value} %'
+			show:true,
+			 textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+        },
+        axisTick: {
+            show: false,
+        },
+        axisLine: {
+            show: true,
+            lineStyle: {
+                color: "rgba(255,255,255,.1	)",
+                width: 1,
+                type: "solid"
+            },
+        },
+        splitLine: {
+            lineStyle: {
+               color: "rgba(255,255,255,.1)",
+            }
+        }
+    }],
+    series: [
+		{
+        type: 'bar',
+        data: [222.59, 268.50, 366.54,446.56, 461.42, 461.62],
+        barWidth:'35%', //柱子宽度
+       // barGap: 1, //柱子之间间距
+        itemStyle: {
+            normal: {
+                color:'#2f89cf',
+                opacity: 1,
+				barBorderRadius: 5,
+            }
+        }
+    }
+		
+	]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+function echarts_2() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echart2'));
+
+       option = {
+  //  backgroundColor: '#00265f',
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: { type: 'shadow'}
+    },
+    grid: {
+        left: '0%',
+		top:'10px',
+        right: '0%',
+        bottom: '4%',
+       containLabel: true
+    },
+    xAxis: [{
+        type: 'category',
+      		data: ['2012','2013', '2014', '2015', '2016', '2017', '2018','2019','2020',],
+        axisLine: {
+            show: true,
+         lineStyle: {
+                color: "rgba(255,255,255,.1)",
+                width: 1,
+                type: "solid"
+            },
+        },
+		
+        axisTick: {
+            show: false,
+        },
+		axisLabel:  {
+                interval: 0,
+               // rotate:50,
+                show: true,
+                splitNumber: 15,
+                textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+    }],
+    yAxis: [{
+        type: 'value',
+        axisLabel: {
+           //formatter: '{value} %'
+			show:true,
+			 textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+        },
+        axisTick: {
+            show: false,
+        },
+        axisLine: {
+            show: true,
+            lineStyle: {
+                color: "rgba(255,255,255,.1	)",
+                width: 1,
+                type: "solid"
+            },
+        },
+        splitLine: {
+            lineStyle: {
+               color: "rgba(255,255,255,.1)",
+            }
+        }
+    }],
+    series: [
+		{
+       
+        type: 'bar',
+        data: [1860.16,2370.65,2895.98,3512.82,5027.54,7116.81,9471.03,12318.86,5785.09],
+        barWidth:'35%', //柱子宽度
+       // barGap: 1, //柱子之间间距
+        itemStyle: {
+            normal: {
+                color:'#27d08a',
+                opacity: 1,
+				barBorderRadius: 5,
+            }
+        }
+    }
+		
+	]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+function echarts_5() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echart5'));
+
+       option = {
+  //  backgroundColor: '#00265f',
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    
+    grid: {
+        left: '0%',
+		top:'10px',
+        right: '0%',
+        bottom: '2%',
+       containLabel: true
+    },
+    xAxis: [{
+        type: 'category',
+      		data: ['2012','2013', '2014', '2015', '2016', '2017', '2018','2019','2020'],
+        axisLine: {
+            show: true,
+         lineStyle: {
+                color: "rgba(255,255,255,.1)",
+                width: 1,
+                type: "solid"
+            },
+        },
+		
+        axisTick: {
+            show: false,
+        },
+		axisLabel:  {
+                interval: 0,
+               // rotate:50,
+                show: true,
+                splitNumber: 15,
+                textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+            },
+    }],
+    yAxis: [{
+        type: 'value',
+        axisLabel: {
+           //formatter: '{value} %'
+			show:true,
+			 textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+                    fontSize: '12',
+                },
+        },
+        axisTick: {
+            show: false,
+        },
+        axisLine: {
+            show: true,
+            lineStyle: {
+                color: "rgba(255,255,255,.1	)",
+                width: 1,
+                type: "solid"
+            },
+        },
+        splitLine: {
+            lineStyle: {
+               color: "rgba(255,255,255,.1)",
+            }
+        }
+    }],
+    series: [{
+        type: 'bar',
+        data: [21401.18,26761.28,32134.92,37630.01,53148.42,74417.43,96858.12,113526.60,61781.49],
+        barWidth:'35%', //柱子宽度
+       // barGap: 1, //柱子之间间距
+        itemStyle: {
+            normal: {
+                color:'#2f89cf',
+                opacity: 1,
+				barBorderRadius: 5,
+            }
+        }
+    }
+	]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+	
+function echarts_4() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echart4'));
+
+    option = {
+	    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            lineStyle: {
+                color: '#dddc6b'
+            }
+        }
+    },
+		    legend: {
+    top:'0%',
+        data:['国内及入境旅游旅行社','旅行社总数'],
+                textStyle: {
+           color: 'rgba(255,255,255,.5)',
+			fontSize:'12',
+        }
+    },
+    grid: {
+        left: '10',
+		top: '30',
+        right: '10',
+        bottom: '10',
+        containLabel: true
+    },
+
+    xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+axisLabel:  {
+                textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+					fontSize:12,
+                },
+            },
+        axisLine: {
+			lineStyle: { 
+				color: 'rgba(255,255,255,.2)'
+			}
+
+        },
+
+   data: ['2015', '2016', '2017', '2018', '2019', '2020']
+
+    }, {
+
+        axisPointer: {show: false},
+        axisLine: {  show: false},
+        position: 'bottom',
+        offset: 20,
+
+       
+
+    }],
+
+    yAxis: [{
+        type: 'value',
+        axisTick: {show: false},
+        axisLine: {
+            lineStyle: {
+                color: 'rgba(255,255,255,.1)'
+            }
+        },
+       axisLabel:  {
+                textStyle: {
+ 					color: "rgba(255,255,255,.6)",
+					fontSize:12,
+                },
+            },
+
+        splitLine: {
+            lineStyle: {
+                 color: 'rgba(255,255,255,.1)'
+            }
+        }
+    }],
+    series: [
+		{
+        name: '国内及入境旅游旅行社',
+        type: 'line',
+         smooth: true,
+        symbol: 'circle',
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+			
+            normal: {
+				color: '#0184d5',
+                width: 2
+            }
+        },
+        areaStyle: {
+            normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgba(1, 132, 213, 0.4)'
+                }, {
+                    offset: 0.8,
+                    color: 'rgba(1, 132, 213, 0.1)'
+                }], false),
+                shadowColor: 'rgba(0, 0, 0, 0.1)',
+            }
+        },
+			itemStyle: {
+			normal: {
+				color: '#0184d5',
+				borderColor: 'rgba(221, 220, 107, .1)',
+				borderWidth: 12
+			}
+		},
+        data: [331, 337,362,311, 467, 634]
+
+    }, 
+{
+        name: '旅行社总数',
+        type: 'line',
+        smooth: true,
+        symbol: 'circle',
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+			
+            normal: {
+				color: '#00d887',
+                width: 2
+            }
+        },
+        areaStyle: {
+            normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgba(0, 216, 135, 0.4)'
+                }, {
+                    offset: 0.8,
+                    color: 'rgba(0, 216, 135, 0.1)'
+                }], false),
+                shadowColor: 'rgba(0, 0, 0, 0.1)',
+            }
+        },
+			itemStyle: {
+			normal: {
+				color: '#00d887',
+				borderColor: 'rgba(221, 220, 107, .1)',
+				borderWidth: 12
+			}
+		},
+        data: [353, 364, 392, 339, 498, 665]
+
+    }, 
+	
+		 ]
+
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+function echarts_6() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('echart6'));
+
+        var dataStyle = {
+	normal: {
+		label: {
+			show: false
+		},
+		labelLine: {
+			show: false
+		},
+		//shadowBlur: 40,
+		//shadowColor: 'rgba(40, 40, 40, 1)',
+	}
+};
+var placeHolderStyle = {
+	normal: {
+		color: 'rgba(255,255,255,.05)',
+		label: {show: false,},
+		labelLine: {show: false}
+	},
+	emphasis: {
+		color: 'rgba(0,0,0,0)'
+	}
+};
+option = {
+	color: ['#0f63d6', '#0f78d6', '#0f8cd6', '#0fa0d6', '#0fb4d6'],
+	tooltip: {
+		show: true,
+		formatter: "{a} : {c} "
+	},
+	legend: {
+		itemWidth: 10,
+        itemHeight: 10,
+		itemGap: 12,
+		bottom: '3%',
+		
+		data: ['住宿', '长途交通', '商品销售', '餐饮', '其他'],
+		textStyle: {
+                    color: 'rgba(255,255,255,.6)',
+                }
+	},
+	
+	series: [
+		{
+		name: '住宿',
+		type: 'pie',
+		clockWise: false,
+		center: ['50%', '42%'],
+		radius: ['59%', '70%'],
+		itemStyle: dataStyle,
+		hoverAnimation: false,
+		data: [{
+			value: 27,
+			name: '01'
+		}, {
+			value: 20,
+			name: 'invisible',
+			tooltip: {show: false},
+			itemStyle: placeHolderStyle
+		}]
+	},
+		{
+		name: '长途交通',
+		type: 'pie',
+		clockWise: false,
+		center: ['50%', '42%'],
+		radius: ['49%', '60%'],
+		itemStyle: dataStyle,
+		hoverAnimation: false,
+		data: [{
+			value: 20,
+			name: '02'
+		}, {
+			value: 30,
+			name: 'invisible',
+			tooltip: {show: false},
+			itemStyle: placeHolderStyle
+		}]
+	}, 
+		{
+		name: '商品销售',
+		type: 'pie',
+		clockWise: false,
+		hoverAnimation: false,
+		center: ['50%', '42%'],
+		radius: ['39%', '50%'],
+		itemStyle: dataStyle,
+		data: [{
+			value: 20,
+			name: '03'
+		}, {
+			value: 35,
+			name: 'invisible',
+			tooltip: {show: false},
+			itemStyle: placeHolderStyle
+		}]
+	},
+		{
+		name: '餐饮',
+		type: 'pie',
+		clockWise: false,
+		hoverAnimation: false,
+		center: ['50%', '42%'],
+		radius: ['29%', '40%'],
+		itemStyle: dataStyle,
+		data: [{
+			value: 18,
+			name: '04'
+		}, {
+			value: 40,
+			name: 'invisible',
+			tooltip: {show: false},
+			itemStyle: placeHolderStyle
+		}]
+	}, 
+		{
+		name: '其他',
+		type: 'pie',
+		clockWise: false,
+		hoverAnimation: false,
+		center: ['50%', '42%'],
+		radius: ['20%', '30%'],
+		itemStyle: dataStyle,
+		data: [{
+			value: 15,
+			name: '05'
+		}, {
+			value: 50,
+			name: 'invisible',
+			tooltip: {show: false},
+			itemStyle: placeHolderStyle
+		}]
+	}, ]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+function echarts_31() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('fb1')); 
+option = {
+   
+	    title: [{
+        text: '年龄分布',
+        left: 'center',
+        textStyle: {
+            color: '#fff',
+			fontSize:'16'
+        }
+
+    }],
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)",
+position:function(p){   //其中p为当前鼠标的位置
+            return [p[0] + 10, p[1] - 10];
+        }
+    },
+    legend: {
+        
+top:'70%',
+       itemWidth: 10,
+        itemHeight: 10,
+        data:['0-20','20-40','40-60','60-80','80以上'],
+                textStyle: {
+            color: 'rgba(255,255,255,.5)',
+			fontSize:'12',
+        }
+    },
+    series: [
+        {
+        	name:'年龄分布',
+            type:'pie',
+			center: ['50%', '42%'],
+            radius: ['40%', '60%'],
+                  color: ['#065aab', '#066eab', '#0682ab', '#0696ab', '#06a0ab','#06b4ab','#06c8ab','#06dcab','#06f0ab'],	
+            label: {show:false},
+			labelLine: {show:false},
+            data:[
+                {value:1, name:'0-20'},
+                {value:4, name:'20-40'},
+                {value:2, name:'40-60'},
+                {value:2, name:'60-80'},
+                {value:1, name:'80以上'},
+            ]
+        }
+    ]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+function echarts_32() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('fb2'));
+option = {
+   
+	    title: [{
+        text: '旅游种类',
+        left: 'center',
+        textStyle: {
+            color: '#fff',
+			fontSize:'16'
+        }
+
+    }],
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)",
+position:function(p){   //其中p为当前鼠标的位置
+            return [p[0] + 10, p[1] - 10];
+        }
+    },
+    legend: {
+        
+    top:'70%',
+       itemWidth: 10,
+        itemHeight: 10,
+        data:['古镇','自然风光','博物馆','图书馆','展览馆','其他'],
+                textStyle: {
+           color: 'rgba(255,255,255,.5)',
+			fontSize:'12',
+        }
+    },
+    series: [
+        {
+        	name:'旅游种类',
+            type:'pie',
+			center: ['50%', '42%'],
+            radius: ['40%', '60%'],
+            color: ['#065aab', '#066eab', '#0682ab', '#0696ab', '#06a0ab','#06b4ab','#06c8ab','#06dcab','#06f0ab'],	
+            label: {show:false},
+			labelLine: {show:false},
+            data:[
+                {value:5, name:'古镇'},
+                {value:1, name:'自然风光'},
+                {value:6, name:'博物馆'},
+                {value:2, name:'图书馆'},
+                {value:1, name:'展览馆'},
+                {value:1, name:'其他'},
+            ]
+        }
+    ]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+function echarts_33() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('fb3'));
+option = {
+	    title: [{
+        text: '兴趣分布',
+        left: 'center',
+        textStyle: {
+            color: '#fff',
+			fontSize:'16'
+        }
+
+    }],
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)",
+position:function(p){   //其中p为当前鼠标的位置
+            return [p[0] + 10, p[1] - 10];
+        }
+    },
+    legend: {
+    top:'70%',
+       itemWidth: 10,
+        itemHeight: 10,
+        data:['传统工艺','村超','村BA','美食','民族特色','其他'],
+                textStyle: {
+            color: 'rgba(255,255,255,.5)',
+			fontSize:'12',
+        }
+    },
+    series: [
+        {
+        	name:'兴趣分布',
+            type:'pie',
+			center: ['50%', '42%'],
+            radius: ['40%', '60%'],
+                   color: ['#065aab', '#066eab', '#0682ab', '#0696ab', '#06a0ab','#06b4ab','#06c8ab','#06dcab','#06f0ab'],	
+            label: {show:false},
+			labelLine: {show:false},
+            data:[
+                {value:2, name:'传统工艺'},
+                {value:3, name:'村超'},
+                {value:1, name:'村BA'},
+                {value:4, name:'美食'},
+                {value:8, name:'民族特色'},
+                {value:1, name:'其他'},
+            ]
+        }
+    ]
+};
+      
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+        window.addEventListener("resize",function(){
+            myChart.resize();
+        });
+    }
+				
+	
+})
+
+
+		
+		
+		
+
+
+		
+
+
+
+
+
+
+
+
+
